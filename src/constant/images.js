3d06fb7d7802174be69f3bb1c75cee980fb85cf5
Aung662001 <107274438+Aung662001@users.noreply.github.com>
2023-03-26 18:47:52 +0630

    change photo

--- src/constant/images.js
+++ src/constant/images.js
@@ -34,7 +34,7 @@ import bolt from "../assets/bolt.png";
 import nb from "../assets/nb.png";
 import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
-import copy from "../assets/skb - Copy.jpg";
+import copy from "../assets/Copy.png";
 
 export default {
   copy,
